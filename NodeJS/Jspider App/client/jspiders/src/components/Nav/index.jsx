ca37b7870db407fe52298a52a22c07e81413b3a9
mrunmaydarpan <mrunmay9776@gmail.com>
2023-04-17 18:10:41 +0530

    update 17/4/23

---
+++ NodeJS/Jspider App/client/jspiders/src/components/Nav/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return <div>
+
+        </div>
+    }
+} / 
---
+++ NodeJS/Jspider App/server/server.js
@@ -0,0 +1,4 @@
+const http = require('http');
+const rootRouter = require('./Router/rootRouter');
+const hostName = "127.0.0.8";
+const PORT = 5050;
